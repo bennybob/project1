8d36496d156e21e9464a1bcfaaf48d0e49ce2d91
Ben Saxon <c3307209@uon.edu.au>
2019-04-05 03:48:06 +0000

    code in progress

--- project1.c
+++ project1.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
-int main() {
-  start assignment
-  return 0;
-  }
+int main()
+{
+   int i, x, k; //i=array pointer, x= switch case, k= encryption key 
+   char str[100];
+   
+  
+   printf("\nPlease enter a message:\t"); //insert your message
+   scanf("%[^\n]%",& str);
+   printf("please enter a key:\t");//enter encryption key
+   scanf("%d" , &k);
+
+   printf("\nPlease choose following options:\n");
+   printf("1 = Encrypt the string.\n");
+   printf("2 = Decrypt the string.\n");
+   scanf("%d", &x);
+
+   //using switch case statements
+   switch(x)
+   {
+   case 1:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] + k; //the key for encryption is added to ASCII value
+
+      printf("\nEncrypted string: %s\n", str);
+      break;
+
+   case 2:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] - k; //the key for encryption is subtracted from the ASCII value
+
+      printf("\nDecrypted string: %s\n", str);
+      break;
+
+   default:
+      printf("\nError\n");
+   }
+   return 0;
+}
