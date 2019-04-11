c95bb22b12275c39ff90b0cc3b80a9599b92858a
Ben Saxon <c3307209@uon.edu>
2019-04-11 05:35:37 +0000

    works...sort of

--- project1.c
+++ project1.c
@@ -1,32 +1,23 @@
-#include <stdio.h>
-
-
-
+#include<stdio.h>
+ 
+ 
 int main()
 {
 	char message[100], ch;
 	int i, key=19;
 	
 	printf("Enter a message to encrypt: ");
-	scanf("%s",& message);
+	gets(message);
 	
-	for(i = 0; message[i] != '\0'; ++i){
+	for(i = 0; message[i] != '\0' ; ++i){
 		ch = message[i];
 		
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch + key;
-			
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
-			
-			message[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
+		if(ch >= 'A' && ch <= 'Z'){
 			ch = ch + key;
 			
 			if(ch > 'Z'){
 				ch = ch - 'Z' + 'A' - 1;
+				
 			}
 			
 			message[i] = ch;
